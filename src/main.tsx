7aefc19594b36145a72043f722b2e155b17c171f
Swizec Teller <swizec@swizec.com>
2023-12-02 07:45:52 -0800

    Put together

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,24 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { routeTree } from "./routeTree.gen";
 import { Router, RouterProvider } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const router = new Router({
-  routeTree,
+    routeTree,
+    context: { queryClient },
 });
 
 // Register things for typesafety
 declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
+    interface Register {
+        router: typeof router;
+    }
 }
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <RouterProvider router={router} />,
+    <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+    </QueryClientProvider>
 );
--- src/pages/story/$id.tsx
+++ src/pages/story/$id.tsx
@@ -1,24 +1,26 @@
-import { FileRoute } from "@tanstack/react-router"
-import { wait } from '../../wait'
+import { FileRoute } from "@tanstack/react-router";
+import { wait } from "../../wait";
 
 const stories = [
-    'Honey never spoils and can remain edible for thousands of years.',
-    'Octopuses have three hearts: two pump blood to the gills, while the third pumps it to the rest of the body.'
-]
+    "Honey never spoils and can remain edible for thousands of years.",
+    "Octopuses have three hearts: two pump blood to the gills, while the third pumps it to the rest of the body.",
+];
 
-export const route = new FileRoute('/story/$id').createRoute({
-    component: ({ useParams }) =>{
-        const { id } = useParams()
-        
-        return <div style={{ background: '#ed553B', padding: '1em'}}>
+export const route = new FileRoute("/story/$id").createRoute({
+    component: ({ useParams }) => {
+        const { id } = useParams();
+
+        return (
+            <div style={{ background: "#ed553B", padding: "1em" }}>
                 <h2>Story {id}</h2>
-                
-                {stories[id-1]}
+
+                {stories[Number(id) - 1]}
             </div>
+        );
     },
     pendingComponent: () => <p>Loading ...</p>,
-    // loader: async () => {
-    //   await wait(5)
-    //   return {}
-    // }
-})
+    load: async () => {
+        await wait(3);
+        return {};
+    },
+});
